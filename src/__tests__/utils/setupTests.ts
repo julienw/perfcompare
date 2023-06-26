13d623ece12c9a800cf49e2630ce74748a85e2ea
Julien Wajsberg <felash@gmail.com>
2023-06-26 16:12:54 +0200

    Add an external resize observer mock

--- src/__tests__/utils/setupTests.ts
+++ src/__tests__/utils/setupTests.ts
@@ -5,8 +5,10 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom';
-
 import React from 'react';
+
+import { ResizeObserver as ResizeObserverPolyfill } from '@juggle/resize-observer';
+import { setupJestCanvasMock } from 'jest-canvas-mock';
 
 import { createStore } from '../../common/store';
 import type { Store } from '../../common/store';
@@ -14,7 +16,7 @@ import {
   createRender,
   createRenderWithRouter,
   createStoreProvider,
-} from '../utils/test-utils';
+} from './test-utils';
 import type { Render, RenderWithRouter } from './test-utils';
 
 const unmockedFetch = global.fetch;
@@ -32,11 +34,20 @@ afterAll(() => {
 });
 
 beforeEach(() => {
+  setupJestCanvasMock();
   jest.useFakeTimers();
   store = createStore();
   render = createRender(store);
   renderWithRouter = createRenderWithRouter(store);
   StoreProvider = createStoreProvider(store);
+
+  if (!('ResizeObserver' in window)) {
+    window.ResizeObserver = ResizeObserverPolyfill;
+  } else {
+    throw new Error(
+      'It looks like this version of jsdom includes a ResizeObserver implementation, you should look whether our mock is still needed.',
+    );
+  }
 });
 
 afterEach(() => {
@@ -45,6 +56,11 @@ afterEach(() => {
   jest.restoreAllMocks();
   jest.runOnlyPendingTimers();
   jest.useRealTimers();
+
+  if (window.ResizeObserver === ResizeObserverPolyfill) {
+    // @ts-expect-error Typescript doesn't know what we mocked it just above.
+    delete window.ResizeObserver;
+  }
 });
 
 export { store, render, renderWithRouter, StoreProvider };
